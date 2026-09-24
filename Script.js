fb624df768308b4a82be89e1099d6ef16181fd8e
Nayan <nayanvmugali26@gmail.com>
2026-09-24 23:05:46 +0530

    added h3 in ui

--- Script.js
+++ Script.js
@@ -5,3 +5,10 @@ for(el of p){
     el.style.color="pink";
 }
 console.log(p);
+let h3=document.querySelectorAll("h3");
+for(el of h3){
+    el.style.backgroundColor="black";
+    el.style.fontSize="28px";
+    el.style.color="pink";
+}
+console.log(h3);
