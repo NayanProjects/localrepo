32019f445b53abdadf6f7395cc4d6f7a3e0ee884
Nayan <nayanvmugali26@gmail.com>
2026-09-24 23:10:54 +0530

    changed font to 22  ui

--- Script.js
+++ Script.js
@@ -8,7 +8,7 @@ console.log(p);
 let h3=document.querySelectorAll("h3");
 for(el of h3){
     el.style.backgroundColor="black";
-    el.style.fontSize="28px";
+    el.style.fontSize="22px";
     el.style.color="pink";
 }
 console.log(h3);
