fa887fd809ce68fdedfdd81d7f76d24efc63784b
Nayan <nayanvmugali26@gmail.com>
2026-09-24 23:27:36 +0530

    added h4 to main branch of script

--- Script.js
+++ Script.js
@@ -5,3 +5,10 @@ for(el of p){
     el.style.color="pink";
 }
 console.log(p);
+let h4=document.querySelectorAll("h4");
+for(el of h4){
+    el.style.backgroundColor="black";
+    el.style.fontSize="28px";
+    el.style.color="pink";
+}
+console.log(h4);
